ccac86f16d1b87c4f233bc3a948528e6ac71d12d
Stella McEwen <mcewen.stella@gmail.com>
2019-04-17 18:39:13 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,7 @@
 def dictionary
   dictionary = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
 end
+
+def word_substituter(tweet)
+  if tweet.includes?(dictionary.keys)
+    
