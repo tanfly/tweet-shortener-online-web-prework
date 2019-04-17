ad553d50d9273e9c23f193ab9364d4bc4b852239
Stella McEwen <mcewen.stella@gmail.com>
2019-04-17 18:34:09 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,3 @@
-# Write your code here.
+def dictionary
+  substitute = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
+  if substitute.keys
